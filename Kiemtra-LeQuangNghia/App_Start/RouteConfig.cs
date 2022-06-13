c948bd5dd858b3aab27977c31146e047c1e5f872
nghia21-1 <107386140+nghia21-1@users.noreply.github.com>
2022-06-14 05:08:23 +0700

    first commit

---
+++ Kiemtra-LeQuangNghia/App_Start/RouteConfig.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Kiemtra_LeQuangNghia
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "SinhVien", action = "ListSinhVien", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Kiemtra-LeQuangNghia/Controllers/SinhVienController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kiemtra_LeQuangNghia.Models;
+
+
+namespace Kiemtra_LeQuangNghia.Controllers
+{
+    public class SinhVienController : Controller
+    {
+        // GET: SinhVien
+        MyDataDataContext data = new MyDataDataContext();
+
+        public object E_SinhVien { get; private set; }
+
+        public ActionResult ListSinhVien()
+        {
+            var all_sinhvien = from ss in data.SinhViens select ss;
+            return View(all_sinhvien);
+        }
+
+         public ActionResult Detail(string id)
+         {
+             var D_sinhvien = data.SinhViens.Where(m => m.MaSV == id).First();
+             return View(D_sinhvien);
+         
+         }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, SinhVien s)
+        {
+            var E_HoTen = collection["HoTen"];
+            var E_Hinh = collection["Hinh"];
+            var E_GioiTinh = collection["GioiTinh"];
+            var E_NgaySinh = Convert.ToDateTime(collection["NgaySinh"]);
+            var E_MaNganh = collection["MaNganh"];
+            if (string.IsNullOrEmpty(E_HoTen))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                s.HoTen = E_HoTen.ToString();
+                s.Hinh = E_Hinh.ToString();
+                s.GioiTinh = E_GioiTinh.ToString();
+                s.NgaySinh = E_NgaySinh;
+                s.MaNganh = E_MaNganh.ToString();
+                data.SinhViens.InsertOnSubmit(s);
+                data.SubmitChanges();
+                return RedirectToAction("ListSach");
+            }
+            return this.Create();
+        }
+
+        public ActionResult Edit(string id)
+        {
+            var E_sinhvien = data.SinhViens.First(m => m.MaSV == id);
+            return View(E_sinhvien);
+        }
+        [HttpPost]
+        public ActionResult Edit(string id, FormCollection collection, SinhVien s)
+        {
+            var E_sinhvien = data.SinhViens.First(m => m.MaSV == id);
+            var E_HoTen = collection["HoTen"];
+            var E_Hinh = collection["Hinh"];    
+            var E_GioiTinh = collection["GioiTinh"];
+            var E_NgaySinh = Convert.ToDateTime(collection["NgaySinh"]);
+            var E_MaNganh = collection["MaNganh"];
+            if (string.IsNullOrEmpty(E_HoTen))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                s.HoTen = E_HoTen.ToString();
+                s.Hinh = E_Hinh.ToString();
+                s.GioiTinh = E_GioiTinh.ToString();
+                s.NgaySinh = E_NgaySinh;
+                s.MaNganh = E_MaNganh.ToString();
+                UpdateModel(E_SinhVien);
+                data.SubmitChanges();
+                return RedirectToAction("ListSinhVien");
+            }
+            return this.Edit(id);
+        }
+
+        public ActionResult Delete(string id)
+        {
+            var D_sinhvien = data.SinhViens.First(m => m.MaSV == id);
+            return View(D_sinhvien);
+        }
+        [HttpPost]
+        public ActionResult Delete(string id, FormCollection collection)
+        {
+            var D_sach = data.SinhViens.Where(m => m.MaSV == id).First();
+            data.SinhViens.DeleteOnSubmit(D_sach);
+            data.SubmitChanges();
+            return RedirectToAction("ListSach");
+        }
+        public string ProcessUpload(HttpPostedFileBase file)
+        {
+            if (file == null)
+            {
+                return "";
+            }
+            file.SaveAs(Server.MapPath("~/Content/images/" + file.FileName));
+            return "/Content/images/" + file.FileName;
+        }
+    }
+}
+
+
+    
+    
